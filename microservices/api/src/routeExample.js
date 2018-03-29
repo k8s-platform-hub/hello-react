3ea961d3521d181267b7d1def9ca957d98c34ac1
Jaison Titus <jaison@hasura.io>
2018-03-29 12:07:26 +0530

    Removed api microservice

--- microservices/api/src/routeExample.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request');
-
-router.route("/").get(function (req, res) {
-  res.send("Hello-React")
-});
-
-module.exports = router;
